4fc0d41e427e5526e546a88961c2f4f597f1554c
hiramekus <hiramekus@gmail.com>
2026-06-08 20:57:44 +0900

    アウトカメラに変更

--- src/components/BarcodeScanner.tsx
+++ src/components/BarcodeScanner.tsx
@@ -24,7 +24,7 @@ const BarcodeScanner = ({ onScan, onClose }: BarcodeScannerProps) => {
 
       try {
         await html5QrCode.start(
-          { facingMode: "user" },
+          { facingMode: "environment" },
           undefined,
           (decodedText) => {
             if (isMounted) onScan(decodedText)
